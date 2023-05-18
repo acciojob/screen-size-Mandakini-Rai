e45dbb4f5c15ceae9ccad88c243c2ab0c10c3d01
Mandakini-Rai <115775774+Mandakini-Rai@users.noreply.github.com>
2023-05-18 18:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const info = document.getElementById('sizeinfo').firstElementChild;
+function getinfo(){
+	info.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+}
